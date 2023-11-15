482a6a41ac1a1f2a430d2242cad018ed517fd451
sadeg1377 <sadeghhadipour1@gmail.com>
2023-11-15 18:51:34 +0330

    add router for StudentSchedule.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import StudentSchedule from "@/components/StudentSchedule.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/student-schedule',
+        name: 'StudentSchedule',
+        component: StudentSchedule
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
